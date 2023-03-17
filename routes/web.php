5fb2aaab4ef40ccf7dc197ff1cc143e579b02229
Egor Nazaikin <nazaikineg@gmail.com>
2023-03-18 02:04:32 +0300

    Task1: Add password checker

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/password/{password}', function($password) {
+    $len_flag = strlen($password) >= 8;
+    $A_flag = false;
+    $a_flag = false;
+    $digit_flag = false;
+    foreach (str_split($password) as $symbol)
+    {
+        if (ctype_lower($symbol)) $a_flag = true;
+        if (ctype_upper($symbol)) $A_flag = true;
+        if (ctype_digit($symbol)) $digit_flag = true;
+    }
+
+    if ($len_flag)
+    {
+        $score = 0;
+        if ($A_flag) $score++;
+        if ($a_flag) $score++;
+        if ($digit_flag) $score++;
+
+        if ($score == 1) echo "Easy";
+        if ($score == 2) echo "Middle";
+        if ($score == 3) echo "GOOOOOOOD";
+    }
+    else 
+    {
+        echo "Password is too short";
+    }
+});
+
+Route::get('password/', function(){
+    echo 'Hmmmmm... I NEED TO SEE YOUR PASSWORD!!!!!!!!!!';
+});
